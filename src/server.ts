7d66cd816ad7f0b29d199d41868cc2e7591fc04c
gan_xingba <gan_xingba@126.com>
2025-01-19 21:20:01 +0800

    base64 test

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,8 @@ import { RawWorkflow, ClientEvent, WorkflowNode } from './types/index.js';
 import { readFileSync } from 'fs';
 import { join } from 'path';
 import { fileURLToPath } from 'url';
-
+// 读取传入的ComfyUI_URL 参数
+const ComfyUI_URL = process.argv[2];
 const __dirname = fileURLToPath(new URL('.', import.meta.url));
 
 // 获取 workflows 目录路径
@@ -23,13 +24,15 @@ const port = 3000;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb', extended: true}));
 
 // POST /api/tagger 端点
 app.post('/api/tagger', async (req: Request, res: Response) => {
     // 初始化ComfyUI客户端
-    const client = new ComfyClient('http://localhost:8188');
+    const client = new ComfyClient(ComfyUI_URL);
     try {
-        const { imagePath } = req.body;
+        const { imagePath, base64 } = req.body;
 
         if (!imagePath) {
             return res.status(400).json({
@@ -40,17 +43,18 @@ app.post('/api/tagger', async (req: Request, res: Response) => {
 
         // 连接到ComfyUI服务器
         await client.connect();
-        
+        const workflow_name = base64 ? 'tagger_v2' : 'tagger';
+        const node_name = base64 ? 'LoadImageFromUrl' : 'LoadImage';
         // 加载tagger工作流
         const workflowsDir = getWorkflowsPath();
-        const taggerPath = join(workflowsDir, 'tagger.json');
+        const taggerPath = join(workflowsDir, `${workflow_name}.json`);
         const tagger = JSON.parse(readFileSync(taggerPath, 'utf-8')) as RawWorkflow;
 
         // 更新工作流中的图片输入节点
         const nodes = Object.values(tagger) as WorkflowNode[];
-        const loadImageNode = nodes.find(node => node.class_type === 'LoadImage');
+        const loadImageNode = nodes.find(node => node.class_type === node_name);
         if (loadImageNode) {
-            loadImageNode.inputs.image = imagePath;
+            loadImageNode.inputs.image = base64 ? base64 : imagePath;
         }
 
         // 创建Promise以等待结果
@@ -60,6 +64,7 @@ app.post('/api/tagger', async (req: Request, res: Response) => {
             });
             
             client.on(ClientEvent.Error, (error: Error) => {
+                console.error('ComfyUI 服务器错误:', error);
                 reject(error);
             });
         });
